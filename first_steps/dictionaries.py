c4bc4b7287ce3443cf987b48a0d4d3bc61fef98b
Elton Campos <eltoncampos36@gmail.com>
2023-02-09 22:12:28 -0300

    dictionaries

---
+++ first_steps/dictionaries.py
@@ -0,0 +1,16 @@
+monthConversions = {
+    "jan": "January",
+    "feb": "February",
+    "mar": "March",
+    "apr": "April",
+    "may": "May",
+    "jun": "June",
+    "jul": "July",
+    "aug": "August",
+    "sep": "September",
+    "oct": "October",
+    "nov": "November",
+    "dec": "December"
+}
+
+print(monthConversions["jan"])
