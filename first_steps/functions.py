743db2e9d88a940bff24e144632759830cd58cc3
Elton Campos <eltoncampos36@gmail.com>
2023-02-09 21:46:34 -0300

    functions and return statement

---
+++ first_steps/functions.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+    return a + b
+
+def sayHi(name):
+    print("Hi " + name)
+
+def cube(num):
+    return num*num*num
+
+sayHi("Mike")
+
+result = cube(8)
+print(result)
+print(sum(1,2))
