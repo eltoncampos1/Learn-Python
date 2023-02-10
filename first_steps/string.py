44bb990aa0ded2d5ef969977111227b617896d86
Elton Campos <eltoncampos36@gmail.com>
2023-02-09 21:06:05 -0300

    strings

---
+++ first_steps/string.py
@@ -0,0 +1,13 @@
+phrase="Python Academy"
+print(phrase)
+
+print(phrase + " is cool")
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("A"))
+print(phrase.replace("Academy", "academy"))
--- giraffe/app.py
+++
@@ -1,9 +0,0 @@
-character_name = "Jhon"
-character_age = 45
-is_male= True
-print("There once was a man named " + character_name + ", " )
-print("he was 35 years old.")
-character_name="Doe"
-character_age=18
-print("He really liked the name " + character_name)
-print("but didn't like being " + character_age )
