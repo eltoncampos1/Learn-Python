d437e322144b2d93f35987b0329f3c4499a7e942
Elton Campos <eltoncampos36@gmail.com>
2023-02-09 21:38:27 -0300

    lists functions

--- first_steps/lists.py
+++ first_steps/lists.py
@@ -1,5 +1,17 @@
 friends=["jhon", "doe", "mary", "moon"]
+lk_numbs= [4,61,9,2,37,0,13, 4]
+friends.extend(lk_numbs)
+friends.append("Chris")
+friends.insert(1, "Diana")
+friends.remove("doe")
+# friends.clear()
+friends.pop()
+lk_numbs.sort()
+lk_numbs.reverse()
+f2= friends.copy()
+print(friends.index(61))
+print(friends.count(4))
 print(friends)
-print(friends[1:3])
-friends[0] = "Mike"
-print(friends)
+print(lk_numbs)
+print(f2)
+
