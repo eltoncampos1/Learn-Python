7ca380d3df3b485a6d6581320da1d0ef044b5a04
Elton Campos <eltoncampos36@gmail.com>
2023-02-09 21:14:02 -0300

    working with numbers

---
+++ first_steps/numbers.py
@@ -0,0 +1,19 @@
+from math import *
+
+print(2)
+print(3+4.8)
+print((3+4)* 7)
+print(10 % 3)
+num=90
+print(num)
+print(num + 7)
+print(str(num) + " has converted to string")
+n2=-5
+print(abs(n2))
+print(pow(9, 2))
+print(max(8, 4))
+print(min(8, 4))
+print(round(9.3))
+print(floor(8.3))
+print(ceil(8.3))
+print(sqrt(9))
