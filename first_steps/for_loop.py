3241ef526fa2566b00023cf7f9adddde204f2249
Elton Campos <eltoncampos36@gmail.com>
2023-02-09 22:29:04 -0300

    for loop

---
+++ first_steps/for_loop.py
@@ -0,0 +1,12 @@
+for letter in "Abcd":
+    print(letter.upper())
+
+friends = ["Jhon", "Doe", "Mary", "Moon"]
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for idx in range(len(friends)):
+    print(friends[idx])
